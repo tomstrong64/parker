ee3deff4c727f7c69d6267597c031ffc4821fe7f
tomstrong64 <tom.strong64@gmail.com>
2023-11-21 20:16:28 +0000

    Renamed sections to zones

--- server/src/tests/teardown.js
+++ server/src/tests/teardown.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
-import { ParkingSections } from '../models/ParkingSections.js';
+import { ParkingZones } from '../models/ParkingZones.js';
 
 export default async () => {
-    await ParkingSections.deleteMany({ _id: { $in: global.ids } });
+    await ParkingZones.deleteMany({ _id: { $in: global.ids } });
     await mongoose.connection.close();
 };
