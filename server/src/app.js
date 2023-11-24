9f3057448b82b25ac49a48e646a89162faad7a84
tomstrong64 <tom.strong64@gmail.com>
2023-11-24 13:37:45 +0000

    Added basic html and js to notify user

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,24 @@
 import express from 'express';
-
 import helmet from 'helmet';
 
-import IndexRouter from './routes/index.router.js';
+import APIRouter from './routes/api.router.js';
+
+// TODO: figure out why the fuck this is necessary
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+/////
 
 const app = express();
 
 app.use(helmet());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', IndexRouter);
+app.use('/api', APIRouter);
 
 export default app;
